f887d1f6b400a612142ad8582c8fe3d156b6c19f
lgelmi <lorenzo.gelmi@prima.it>
2020-12-03 13:33:11 +0100

    Fix 3.1.

--- day3/day3.py
+++ day3/day3.py
@@ -1,8 +1,7 @@
-import dataclasses
-import functools
+import itertools
 
 from pathlib import Path
-from typing import List, Iterator
+from typing import List, Iterator, Iterable, Sized
 
 
 def read_input() -> List[str]:
@@ -11,11 +10,20 @@ def read_input() -> List[str]:
 
 
 def traverse(geology, right, down) -> str:
-    return "".join(level[position] for level, position in zip(geology[::down], range(0, len(geology[0]), right)))
+    return "".join(
+        level[position]
+        for level, position in zip(geology[down::down], module_count(right, right, module=len(geology[0])))
+    )
+
+
+def module_count(start, step: int = 1, module=1) -> Iterable[int]:
+    count = itertools.count(start, step)
+    while True:
+        yield next(count) % module
 
 
 def solve_1(values: List[str]):
-    traversed = traverse(values, 3, 1)
+    traversed = traverse(values, right=3, down=1)
     print(traversed)
     print(traversed.count("#"))
 
@@ -26,5 +34,7 @@ def solve_2(values: List[str]):
 
 if __name__ == "__main__":
     values = read_input()
+
+    print([v for _,v in zip(range(5), module_count(1, 3))])
     solve_1(values)
     solve_2(values)
